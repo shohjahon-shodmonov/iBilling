8e9fc1df9c9dfc9fc7622c70ff35ce80731452b5
Shohjahon <sh.shodmonov1@gmail.com>
2022-04-08 16:00:33 +0500

    profile info

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:ibilling/core/components/size_config.dart';
+import 'package:ibilling/core/constants/const.dart';
 import 'package:ibilling/core/widgets/appbar.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -6,9 +9,96 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       appBar: AppBarWidget.appbar("Profile", false),
-      
+      body: Column(
+        children: [
+          Container(
+            height: getHeight(188),
+            width: getWidth(343),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: ColorConstants.kDarkContainerBg,
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SvgPicture.asset(
+                        'assets/icons/account-circle.svg',
+                        height: getHeight(60),
+                        width: getWidth(60),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          "Otabek Abdusamatov",
+                          style: TextStyle(
+                            color: ColorConstants.kLightGreenButtonBgColor,
+                            fontSize: 19,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          "Graphic Designer | IQ Education",
+                          style: TextStyle(
+                            color: ColorConstants.textColorInCard,
+                            fontSize: 15,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    profileTitle("Date of birth:  "),
+                    profileInfo("16.01.2001"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    profileTitle("Phone number: "),
+                    profileInfo("+998901234656"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    profileTitle("E-mail: "),
+                    profileInfo("predictor@gmail.com"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Text profileInfo(String info) {
+    return Text(
+      info,
+      style: const TextStyle(
+        color: ColorConstants.textColorInCardGrey,
+        fontSize: 14,
+      ),
+    );
+  }
+
+  Text profileTitle(String title) {
+    return Text(
+      title,
+      style: const TextStyle(
+        color: ColorConstants.textColorInCard,
+        fontSize: 15,
+      ),
     );
   }
 }
