fc8f5a9b7f1d4653a2e2c6b2b10be2554f85083f
Shohjahon <sh.shodmonov1@gmail.com>
2022-04-09 11:43:00 +0500

    profile screen

--- lib/cubit/language/language_cubit.dart
+++ lib/cubit/language/language_cubit.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ibilling/cubit/language/language_state.dart';
 
 class LanguageCubit extends Cubit<LanguageState> {
   LanguageCubit() : super(LanguageInitial());
 
-  static const List languageList = [
+  bool isActive = false;
+  List languageList = [
     {
       'flag': 'assets/icons/uz.svg',
       'language': "O'zbek (Lotin)",
@@ -20,9 +20,13 @@ class LanguageCubit extends Cubit<LanguageState> {
     },
   ];
 
-  ChangeLanguage() {
-    
+  String changeLanguage(String owned) {
+    isActive = !isActive;
+    if (isActive == true) {
+      owned = 'active';
+    } else {
+      owned = 'inactive';
+    }
+    return owned;
   }
-
-  
 }
--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:ibilling/core/components/size_config.dart';
 import 'package:ibilling/core/constants/const.dart';
 import 'package:ibilling/core/widgets/appbar.dart';
+import 'package:ibilling/cubit/language/language_cubit.dart';
+import 'package:ibilling/cubit/language/language_state.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -10,79 +13,108 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return Scaffold(
-      appBar: AppBarWidget.appbar("Profile", false),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(15),
-            height: getHeight(188),
-            width: getWidth(343),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: ColorConstants.kDarkContainerBg,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SvgPicture.asset(
-                        'assets/icons/account-circle.svg',
-                        height: getHeight(60),
-                        width: getWidth(60),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(
-                          "Otabek Abdusamatov",
-                          style: TextStyle(
-                            color: ColorConstants.kLightGreenButtonBgColor,
-                            fontSize: 19,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        Text(
-                          "Graphic Designer | IQ Education",
-                          style: TextStyle(
-                            color: ColorConstants.textColorInCard,
-                            fontSize: 16,
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    profileTitle("Date of birth:  "),
-                    profileInfo("16.01.2001"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    profileTitle("Phone number: "),
-                    profileInfo("+998901234656"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    profileTitle("E-mail: "),
-                    profileInfo("predictor@gmail.com"),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+    return BlocProvider(
+      create: (context) => LanguageCubit(),
+      child: BlocConsumer<LanguageCubit, LanguageState>(
+        listener: (context, state) {},
+        builder: (context, state) => profileScaffold(context),
       ),
     );
+  }
+
+  Scaffold profileScaffold(BuildContext context) {
+    return Scaffold(
+    appBar: AppBarWidget.appbar("Profile", false),
+    body: Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(15),
+          height: getHeight(188),
+          width: getWidth(343),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: ColorConstants.kDarkContainerBg,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SvgPicture.asset(
+                      'assets/icons/account-circle.svg',
+                      height: getHeight(60),
+                      width: getWidth(60),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        "Otabek Abdusamatov",
+                        style: TextStyle(
+                          color: ColorConstants.kLightGreenButtonBgColor,
+                          fontSize: 19,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        "Graphic Designer | IQ Education",
+                        style: TextStyle(
+                          color: ColorConstants.textColorInCard,
+                          fontSize: 16,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  profileTitle("Date of birth:  "),
+                  profileInfo("16.01.2001"),
+                ],
+              ),
+              Row(
+                children: [
+                  profileTitle("Phone number: "),
+                  profileInfo("+998901234656"),
+                ],
+              ),
+              Row(
+                children: [
+                  profileTitle("E-mail: "),
+                  profileInfo("predictor@gmail.com"),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            color: ColorConstants.kDarkContainerBg,
+            child: ListTile(
+              title: Text(
+                context.watch<LanguageCubit>().languageList[0]['language'],
+                style: TextStyle(
+                  color: ColorConstants.textColorInCard,
+                  fontSize: 15,
+                ),
+              ),
+              trailing: SvgPicture.asset(context.watch<LanguageCubit>().languageList[0]['flag'],
+            ),
+          ),
+        ),),
+      ],
+    ),
+  );
   }
 
   Text profileInfo(String info) {
